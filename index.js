52f0cd40e71677b64e7bc435eaacfb11539c2241
LakjeewaWije <layerswije@gmail.com>
2024-11-13 18:17:24 +0000

    Formatted :  index.js

--- index.js
+++ index.js
@@ -73,34 +73,6 @@ function appendRentData(data) {
     gridContainer.append()
 }
 
-// $("#searchKey").on("keyup", function () {
-//     var val = $.trim(this.value);
-//     console.log("val ", val);
-
-//     if (!val) return appendRentData(rentData);
-//     // Configure Fuse.js options 
-//     options = {
-//         keys: ['name', 'location', 'category'], // Fields to search 
-//         threshold: 0.3 // Adjust this value for stricter or looser matches 
-//     };
-
-//     // Create a new instance of Fuse 
-//     const fuse = new Fuse(rentData, options);
-
-//     const sortedRentals = fuse.search(val);
-
-//     console.log("sortedRentals ", sortedRentals);
-
-//     let tempRental = [];
-
-//     sortedRentals.forEach((data) => {
-//         tempRental.push(data.item);
-//     })
-
-
-//     appendRentData(tempRental);
-// });
-
 function filterProperties() {
     // search query
     var searchKey = $('#searchKey').val();
